4b8277bd5f69f8d9acedb47f07904088924738a1
Tasha Lane <designtmg77@gmail.com>
2020-08-26 12:00:53 -0500

    initial bikestore files

---
+++ BikeStore.cs
@@ -0,0 +1,12 @@
+namespace Store
+{
+
+    public class BikeStore
+    {
+
+        public string Name = "Bud's Bicycles";
+        public string Location = "333 N Main St";
+
+        public string Hours = " 7am til 5pm ";
+    }
+}
